a510a98d460df6defae87503de9d4135de04f9b5
김지헌 <youwa65@gmail.com>
2022-11-07 19:59:41 +0900

    제네릭

--- app.ts
+++ app.ts
@@ -1,12 +1,20 @@
-//<T>,<V> 제네릭을 받는다
-type name = {
-  //타입스크립트가 유추해서 보여줌
-  <TypePlaceholder>(arr: TypePlaceholder[]): TypePlaceholder; //arr은 숫자의 배열이고 아무것도 반환X
+type Player<E> = {
+  name: string;
+  extraInfo: E;
 };
 
-const superPrint: name = (arr) => arr[0];
+type NicoExtra = {
+  favFood: string;
+};
+type NicoPlayer = Player<NicoExtra>;
 
-const a = superPrint([1, 2, 3, 4]);
-const x = superPrint([true, false, true]);
-const c = superPrint(['a', 'b', 'x']);
-const w = superPrint([1, 2, true, false, 'ss']);
+const nico: NicoPlayer = {
+  name: 'nico',
+  extraInfo: {
+    favFood: 'nnoon',
+  },
+};
+const lynn: Player<null> = {
+  name: 'lynn',
+  extraInfo: null,
+};
